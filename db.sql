286a2e43682700f9df91cd3ea1fa432beff2e980
Nicolas Mormiche <nicolas160796@gmail.com>
2021-10-21 16:03:17 +0200

    [BD] Ajout des descriptions des albums

--- db.sql
+++ db.sql
@@ -19,7 +19,7 @@
 
 
 
-INSERT INTO album(id, name, created_at) VALUES (1, 'Lego', '2021-01-22');
+INSERT INTO album(id, name, description, created_at) VALUES (1, 'Lego', "The Lego Group, ou Lego Group, est une entreprise danoise fabriquant des jeux dont la gamme de produits la plus connue est Lego, ils sont constitués de briques élémentaires à assembler.", '2021-01-22');
 INSERT INTO item(name, album_id) VALUES ('Millennium Falcon', 1);
 INSERT INTO item(name, album_id) VALUES ('Stormtrooper', 1);
 INSERT INTO item(name, album_id) VALUES ('BB-8', 1);
@@ -28,7 +28,7 @@ INSERT INTO item(name, album_id) VALUES ('Imperial Destroyer', 1);
 INSERT INTO item(name, album_id) VALUES ('AT-AT', 1);
 INSERT INTO item(name, album_id) VALUES ('The Mandalorian', 1);
 
-INSERT INTO album(id, name, created_at) VALUES (2, 'Jeux vidéos', '2021-09-09');
+INSERT INTO album(id, name, description, created_at) VALUES (2, 'Jeux vidéos', "Jeu électronique doté d'une interface utilisateur permettant une interaction humaine ludique en générant un retour visuel à l'utilisateur.", '2021-09-09');
 INSERT INTO item(name, album_id) VALUES ('Last Of Us 2', 2);
 INSERT INTO item(name, album_id) VALUES ('Destiny 2', 2);
 INSERT INTO item(name, album_id) VALUES ('CyberPunk', 2);
@@ -43,7 +43,7 @@ INSERT INTO item(name, album_id) VALUES ('Metal Gear Solid', 2);
 INSERT INTO item(name, album_id) VALUES ('Red Dead Redemption 2', 2);
 INSERT INTO item(name, album_id) VALUES ('Grand Theft Auto 5', 2);
 
-INSERT INTO album(id, name, created_at) VALUES (3, 'Moog', '2021-05-16');
+INSERT INTO album(id, name, description, created_at) VALUES (3, 'Moog', "Moog Music Inc. est une société américaine spécialisée dans la fabrication d'effets et d'instruments de musique électroniques.", '2021-05-16');
 INSERT INTO item(name, album_id) VALUES ('Grandmother', 3);
 INSERT INTO item(name, album_id) VALUES ('Mother-32', 3);
 INSERT INTO item(name, album_id) VALUES ('Minitaur', 3);
@@ -51,7 +51,7 @@ INSERT INTO item(name, album_id) VALUES ('DFAM', 3);
 INSERT INTO item(name, album_id) VALUES ('Theremini', 3);
 INSERT INTO item(name, album_id) VALUES ('Subharmonicon', 3);
 
-INSERT INTO album(id, name, created_at) VALUES (4, 'Roland', '2019-06-03');
+INSERT INTO album(id, name, description, created_at) VALUES (4, 'Roland', "Roland Corporation, connue sous le nom de Roland, est une entreprise japonaise fabriquant des instruments de musique électroniques.", '2019-06-03');
 INSERT INTO item(name, album_id) VALUES ('TB-03', 4);
 INSERT INTO item(name, album_id) VALUES ('TR-09', 4);
 INSERT INTO item(name, album_id) VALUES ('TB-303', 4);
@@ -65,7 +65,7 @@ INSERT INTO item(name, album_id) VALUES ('Fantom 6', 4);
 INSERT INTO item(name, album_id) VALUES ('RD-2000', 4);
 INSERT INTO item(name, album_id) VALUES ('TR-909', 4);
 
-INSERT INTO album(id, name, created_at) VALUES (5, 'Daft Punk', '2019-07-30');
+INSERT INTO album(id, name, description, created_at) VALUES (5, 'Daft Punk', "Daft Punk est un groupe de musique électronique français, originaire de Paris. Composé de Thomas Bangalter et Guy-Manuel de Homem-Christo.", '2019-07-30');
 INSERT INTO item(name, album_id) VALUES ('Random Access Memories', 5);
 INSERT INTO item(name, album_id) VALUES ('Discovery', 5);
 INSERT INTO item(name, album_id) VALUES ('Homework', 5);
@@ -77,7 +77,7 @@ INSERT INTO item(name, album_id) VALUES ('Greatest Hits', 5);
 INSERT INTO item(name, album_id) VALUES ('Deep Cuts', 5);
 INSERT INTO item(name, album_id) VALUES ('Daft Club', 5);
 
-INSERT INTO album(id, name, created_at) VALUES (6, 'Genshin Impact', '2021-10-18');
+INSERT INTO album(id, name, description, created_at) VALUES (6, 'Genshin Impact', "Explorez sept pays et rencontrez des compagnons de voyage aux capacités uniques. Entrez dans le monde fantastique et battez-vous grâce aux éléments.", '2021-10-18');
 INSERT INTO item(name, album_id) VALUES ('Razor', 6);
 INSERT INTO item(name, album_id) VALUES ('Kenya', 6);
 INSERT INTO item(name, album_id) VALUES ('Xiao', 6);
